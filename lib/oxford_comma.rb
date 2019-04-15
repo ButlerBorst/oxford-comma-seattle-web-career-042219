25c5c21bd671c00e4c4b3c69d1da3260812422b0
Alex Borst <butler.borst@gmail.com>
2019-04-15 21:39:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
 end
 
 def oxford_comma(array)
-if array.length < 2
+if array.length <= 2
   result = array.join(" and ")
 else
   puts "#{array[0..-1].join(', ')} and #{array.last}"
