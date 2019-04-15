d009a3cf4a3cfabe121875090c0c4c458060d07a
Alex Borst <butler.borst@gmail.com>
2019-04-15 21:42:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,6 @@ def oxford_comma(array)
 if array.length <= 2
   array.join(" and ")
 else
-  return "#{array[0..-1].join(', ')} and #{array.last}"
+  return "#{array[0..-2].join(', ')} and #{array.last}"
 end
 end
