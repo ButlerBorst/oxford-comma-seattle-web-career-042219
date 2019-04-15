39b4addeef0d3b8fa8a6a1b768825ec7da175855
Alex Borst <butler.borst@gmail.com>
2019-04-15 21:33:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,12 +7,8 @@ def oxford_comma(array)
 end
 
 def oxford_comma(array)
-case array.length 
-when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
+if array.length == 1
+  result = array[0]
+else
+  result = "#{array[0, array.length].join(', ')} and #{array.last}"
 end
