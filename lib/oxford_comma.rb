6447e0aada63cc1bbee2000f32d1cc086d470ca2
Alex Borst <butler.borst@gmail.com>
2019-04-15 21:45:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+array.join
+end
 
+
+
+def oxford_comma(array)
+if array.length <= 2
+  array.join(" and ")
+else
+  return "#{array[0..-2].join(', ')}, and #{array.last}"
+end
 end
